4747a2a362148d48fd92bd1ef284357659d7d207
Zach Howes <zhowes14@icloud.com>
2022-05-17 01:03:29 -0400

    Added all code for part 1. Created a view for turtle rock. Created multiple components then put them together

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            MapView() .ignoresSafeArea(edges: .top).frame(height: 300)
+            
+            CircleImage().offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+               
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }.font(.subheadline).foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock").font(.title2)
+                Text("Descriptive text goes here.")
+            }.padding()
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
